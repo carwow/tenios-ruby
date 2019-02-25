501cc2db64754a98456312a1cbad6f533ccb08e1
Christian Gregg <c_arlt@hotmail.com>
2019-02-25 11:46:59 +0000

    Create helper methods for building blocks (#1)
    Adds the following methods to facilitate easier building of response
    blocks:
    
    - `Tenios.blocks`
    - `Tenios::Blocks#announce`
    - `Tenios::Blocks#bridge`
    - `Tenios::Blocks#collect_digits`
    - `Tenios::Blocks#collect_speech`
    - `Tenios::Blocks#hang_up`
    - `Tenios::Blocks#routing_plan`
    - `Tenios::Blocks#say`
    
    The idea behind this is to be able to create responses in the following
    manner:
    
    ```ruby
    Tenios.blocks do |blocks|
      blocks.announce(announcement: 'forward_to_customer_services')
      blocks.bridge(mode: Tenios::Blocks::Bridge::SEQUENTIAL) do |bridge|
        bridge.with_destination(
          Tenios::Blocks::Bridge::EXTERNAL_NUMBER,
          "+441234567890",
          10
        )
      end
    end
    ```

--- lib/tenios/blocks.rb
+++ lib/tenios/blocks.rb
@@ -14,6 +14,90 @@ module Tenios
       self
     end
 
+    def announce(announcement:, standard: false)
+      block = Announcement.new(
+        announcement: announcement,
+        standard: standard
+      )
+
+      add(block)
+    end
+
+    def bridge(mode:, timeout: nil)
+      block = Bridge.new(
+        mode: mode,
+        timeout: timeout
+      )
+
+      yield(block) if block_given?
+
+      add(block)
+    end
+
+    def collect_digits(
+      announcement:,
+      standard_announcement:,
+      error_announcement:,
+      standard_error_announcement:,
+      variable:,
+      min_digits:,
+      max_digits:,
+      terminator:,
+      max_tries:,
+      timeout:
+    )
+      block = CollectDigits.new(
+        announcement: announcement,
+        standard_announcement: standard_announcement,
+        error_announcement: error_announcement,
+        standard_error_announcement: standard_error_announcement,
+        variable: variable,
+        min_digits: min_digits,
+        max_digits: max_digits,
+        terminator: terminator,
+        max_tries: max_tries,
+        timeout: timeout
+      )
+
+      add(block)
+    end
+
+    def collect_speech(
+      announcement:,
+      missing_input_announcement:,
+      language:,
+      variable:,
+      max_tries:
+    )
+      block = CollectSpeech.new(
+        announcement: announcement,
+        missing_input_announcement: missing_input_announcement,
+        language: language,
+        variable: variable,
+        max_tries: max_tries
+      )
+
+      add(block)
+    end
+
+    def hang_up(cause:)
+      block = HangUp.new(cause: cause)
+
+      add(block)
+    end
+
+    def routing_plan(routing_plan:)
+      block = RoutingPlan.new(routing_plan: routing_plan)
+
+      add(block)
+    end
+
+    def say(text:, voice:, ssml:)
+      block = Say.new(text: text, voice: voice, ssml: ssml)
+
+      add(block)
+    end
+
     def as_json(*)
       {
         blocks: @blocks.map(&:as_json)
--- spec/tenios/blocks_spec.rb
+++ spec/tenios/blocks_spec.rb
@@ -35,4 +35,119 @@ RSpec.describe Tenios::Blocks do
       expect(json).to eq(expected_json)
     end
   end
+
+  describe '#announce' do
+    it 'adds an announcement block' do
+      instance.announce(announcement: 'hello', standard: true)
+
+      announcement = Tenios::Blocks::Announcement.new(
+        announcement: 'hello',
+        standard: true
+      )
+
+      blocks = described_class.new
+      blocks.add(announcement)
+
+      expect(instance.as_json).to eq(blocks.as_json)
+    end
+  end
+
+  describe '#bridge' do
+    it 'adds a bridge block' do
+    end
+
+    it 'yields an instance of Tenios::Blocks::Bridge' do
+      expect { |b| instance.bridge(mode: Tenios::Blocks::Bridge::SEQUENTIAL, &b) }
+        .to yield_control.once
+
+      expect { |b| instance.bridge(mode: Tenios::Blocks::Bridge::SEQUENTIAL, &b) }
+        .to yield_with_args(Tenios::Blocks::Bridge)
+    end
+  end
+
+  describe '#collect_digits' do
+    it 'adds a collect_digits block' do
+      instance.collect_digits(
+        announcement: 'announcement',
+        standard_announcement: true,
+        error_announcement: 'error_announcement',
+        standard_error_announcement: false,
+        variable: 'variable',
+        min_digits: 1,
+        max_digits: 32,
+        terminator: '#',
+        max_tries: 10,
+        timeout: 300
+      )
+
+      collect_digits = Tenios::Blocks::CollectDigits.new(
+        announcement: 'announcement',
+        standard_announcement: true,
+        error_announcement: 'error_announcement',
+        standard_error_announcement: false,
+        variable: 'variable',
+        min_digits: 1,
+        max_digits: 32,
+        terminator: '#',
+        max_tries: 10,
+        timeout: 300
+      )
+
+      blocks = described_class.new
+      blocks.add(collect_digits)
+
+      expect(instance.as_json).to eq(blocks.as_json)
+    end
+  end
+
+  describe '#collect_speech' do
+    pending
+  end
+
+  describe '#hang_up' do
+    it 'adds a hang_up block' do
+      instance.hang_up(cause: Tenios::Blocks::HangUp::NO_ANSWER)
+
+      hang_up = Tenios::Blocks::HangUp.new(
+        cause: Tenios::Blocks::HangUp::NO_ANSWER
+      )
+
+      blocks = described_class.new
+      blocks.add(hang_up)
+
+      expect(instance.as_json).to eq(blocks.as_json)
+    end
+  end
+
+  describe '#routing_plan' do
+    it 'adds a routing_plan block' do
+      instance.routing_plan(routing_plan: 'hello')
+
+      routing_plan = Tenios::Blocks::RoutingPlan.new(
+        routing_plan: 'hello'
+      )
+
+      blocks = described_class.new
+      blocks.add(routing_plan)
+
+      expect(instance.as_json).to eq(blocks.as_json)
+    end
+  end
+
+  describe '#say' do
+    it 'adds a say block' do
+      instance.say(text: 'hello', voice: 'voice', ssml: false)
+
+      say = Tenios::Blocks::Say.new(
+        text: 'hello',
+        voice: 'voice',
+        ssml: false
+      )
+
+      blocks = described_class.new
+      blocks.add(say)
+
+      expect(instance.as_json).to eq(blocks.as_json)
+    end
+  end
 end
